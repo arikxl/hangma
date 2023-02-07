a21313b6fc66ff75d44c5efa2afdf7126a8a4cea
Arik Alexandrov <arikxl@gmail.com>
2023-02-07 23:56:03 +0200

    drawing1

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,24 @@
+import { useState } from 'react'
+import { Drawing } from './components/Drawing';
+import { Keyboard } from './components/Keyboard';
+import { Word } from './components/Word';
+import words from './data/words.json'
 
 
 function App() {
 
+  const [wordToGuess, setWordToGuess] = useState(() => {
+    return words[~~(Math.random() * words.length)];
+  });
+  const [guessedLetters, setGuessedLetters] = useState<string[]>([]);
+
   return (
-    <div className="App">
-      arik
-    </div>
+    <main className="App">
+      <h3>lose win</h3>
+      <Drawing />
+      {/* <Word /> */}
+      {/* <Keyboard /> */}
+    </main>
   )
 }
 
